ae2fc0544b72d3c9715e59c65fc300c2dfc3c00b
Junaid4ever <110425866+Junaid4ever@users.noreply.github.com>
2026-08-19 00:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# main.py (minimal – बस यह चेक करें कि App deploy हो रहा है)
 from fastapi import FastAPI
-
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"status": "ok", "message": "Zoom Worker Server is running"}
+def root():
+    return {"status": "ok"}
